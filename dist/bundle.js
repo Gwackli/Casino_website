3768903a1daa383c507d37c8c8cd2843d5d395e9
Gian-Luca Held <gian-luca.held@bluewin.ch>
2022-12-26 19:55:08 +0100

    claim func

--- dist/bundle.js
+++ dist/bundle.js
@@ -222,8 +222,24 @@ async function bet() {
     }
 }
 
+async function claim() {
+    if (typeof window.ethereum !== "undefined") {
+        const address = "0xED7c57D4049F947D2c31e22Be5DF50d33c603807"
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(address, abi, signer)
+        try {
+            //await contract.send_win("0x57e37d04D3FdDF41987C518F5E5593Cf70309362", "1000000000000000000");
+            await contract.claim();
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
 module.exports = {
-    connect, bet,
+    connect, bet, claim,
 };
 
 },{"./data":1,"ethers":151}],3:[function(require,module,exports){
